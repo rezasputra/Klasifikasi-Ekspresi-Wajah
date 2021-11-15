82313baf4068f1d4d4fbd471361781fca5e00c07
rezasputra <eca.lucky7@gmail.com>
2021-11-15 10:03:57 +0700

    hero

--- Procfile
+++ Procfile
@@ -4,7 +4,8 @@
 {\*\expandedcolortbl;;\cssrgb\c16078\c16078\c16078;\cssrgb\c94902\c94902\c94902;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \deftab720
-\pard\pardeftab720\partightenfactor0
+\pard\pardeftab720\sl377\partightenfactor0
 
 \f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 web: sh setup.sh && streamlit run app.py}
+\outl0\strokewidth0 \strokec2 web: sh setup.sh && streamlit run app.py\cb1 \
+}
--- setup.sh
+++ setup.sh
@@ -4,15 +4,19 @@
 {\*\expandedcolortbl;;\cssrgb\c16078\c16078\c16078;\cssrgb\c94902\c94902\c94902;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \deftab720
-\pard\pardeftab720\sl377\partightenfactor0
+\pard\pardeftab720\partightenfactor0
 
 \f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 mkdir -p ~/.streamlit/\cb1 \
-\cb3 echo "\\\cb1 \
-\cb3 [server]\\n\\\cb1 \
-\cb3 headless = true\\n\\\cb1 \
-\cb3 port = $PORT\\n\\\cb1 \
-\cb3 enableCORS = false\\n\\\cb1 \
-\cb3 \\n\\\cb1 \
-\cb3 " > ~/.streamlit/config.toml\cb1 \
-}
+\outl0\strokewidth0 \strokec2 mkdir -p ~/.streamlit/\
+\
+echo "\\\
+[general]\\n\\\
+email = \\"email@domain\\"\\n\\\
+" > ~/.streamlit/credentials.toml\
+\
+echo "\\\
+[server]\\n\\\
+headless = true\\n\\\
+enableCORS=false\\n\\\
+port = $PORT\\n\\\
+" > ~/.streamlit/config.toml}
