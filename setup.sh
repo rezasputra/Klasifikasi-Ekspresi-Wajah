91200f0aaeee24ebcb848c0f57fc8a994f6da412
Reza Syahputra <66559322+rezasputra@users.noreply.github.com>
2021-11-15 10:23:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
-echo "\\\
-[general]\\n\\\
-email = \\"email@domain\\"\\n\\\
-" > ~/.streamlit/credentials.toml\
-\
-echo "\\\
-[server]\\n\\\
-headless = true\\n\\\
-enableCORS=false\\n\\\
-port = $PORT\\n\\\
-" > ~/.streamlit/config.toml}
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"email@domain\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
