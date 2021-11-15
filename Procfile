0e0365da4cc2accdbb125dd2c61ddaf60197abd3
rezasputra <eca.lucky7@gmail.com>
2021-11-15 09:47:18 +0700

    heroku

---
+++ Procfile
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red31\green31\blue31;\red239\green239\blue239;}
+{\*\expandedcolortbl;;\cssrgb\c16078\c16078\c16078;\cssrgb\c94902\c94902\c94902;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 web: sh setup.sh && streamlit run app.py}
---
+++ setup.sh
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red31\green31\blue31;\red239\green239\blue239;}
+{\*\expandedcolortbl;;\cssrgb\c16078\c16078\c16078;\cssrgb\c94902\c94902\c94902;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl377\partightenfactor0
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 mkdir -p ~/.streamlit/\cb1 \
+\cb3 echo "\\\cb1 \
+\cb3 [server]\\n\\\cb1 \
+\cb3 headless = true\\n\\\cb1 \
+\cb3 port = $PORT\\n\\\cb1 \
+\cb3 enableCORS = false\\n\\\cb1 \
+\cb3 \\n\\\cb1 \
+\cb3 " > ~/.streamlit/config.toml\cb1 \
+}
